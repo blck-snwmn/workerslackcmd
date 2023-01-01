e93a6bea0b5dc786870bcc068fefd9a2cf3dbc33
blck-snwmn <whitesnowmancreator@gmail.com>
2023-01-02 04:09:30 +0900

    Parse webhook request

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,45 @@ export interface Env {
 
 }
 
+interface SlackWebhookRequest {
+	token: string | null;
+	team_id: string | null;
+	team_domain: string | null;
+	channel_id: string | null;
+	channel_name: string | null;
+	user_id: string | null;
+	user_name: string | null;
+	command: string | null;
+	text: string | null;
+	api_app_id: string | null;
+	is_enterprise_install: string | null;
+	response_url: string | null;
+	trigger_id: string | null;
+}
+
 export default {
 	async fetch(
 		request: Request,
 		env: Env,
 		ctx: ExecutionContext
 	): Promise<Response> {
+		const form = await request.formData();
+		const sreq: SlackWebhookRequest = {
+			token: form.get("token"),
+			team_id: form.get("team_id"),
+			team_domain: form.get("team_domain"),
+			channel_id: form.get("channel_id"),
+			channel_name: form.get("channel_name"),
+			user_id: form.get("user_id"),
+			user_name: form.get("user_name"),
+			command: form.get("command"),
+			text: form.get("text"),
+			api_app_id: form.get("api_app_id"),
+			is_enterprise_install: form.get("is_enterprise_install"),
+			response_url: form.get("response_url"),
+			trigger_id: form.get("trigger_id"),
+		};
+		console.log(sreq)
 		return new Response("Hello World!");
 	},
 };
